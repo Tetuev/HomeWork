bb13ad90e7c3eddfe5836c42a099c3c047036a93
Roman <56489093+Tetuev@users.noreply.github.com>
2019-11-18 15:50:17 +0300

    Add files via upload

--- CollectionTask3.java
+++ CollectionTask3.java
@@ -1,21 +1,24 @@
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.LinkedList;
 
 public class CollectionTask3 {
     public static void main(String[] args) {
-        ArrayList<Integer> set = new ArrayList<Integer>();
+        ArrayList<Integer> set = new ArrayList<>();
         for (int i = 0; i < 20; i++) {
             int n = (int) (Math.random() * 20 + 1);
-            set.add(i, n);
+            set.add(i,n);
         }
         System.out.println(set);
-        for (int i = 0; i < 20; i++) {  //Выдает ошибку
-            int n = set.get(i);
-            if (n > 10) {
-                set.remove(i);
+        Iterator<Integer> iter = set.iterator();
+        while (iter.hasNext()){
+            Integer num = iter.next();
+            if(num > 10) {
+                iter.remove();
             }
-
         }
+
+
         System.out.println(set);
 
     }
